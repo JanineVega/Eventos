f86bda12f9cc56388a2658e53dbf64088b14cb01
Janine Vega <osmary.vega@gmail.com>
2017-03-15 13:09:52 -0500

    Subiendo archivos

---
+++ assets/js/app.js
@@ -0,0 +1,8 @@
+window.addEventListener("load", function(){
+
+document.getElementById("gato3").addEventListener("click", otraPagina);
+function otraPagina(){
+  window.location.href ="https://www.google.com.pe/search?q=stress&espv=2&source=lnms&tbm=isch&sa=X&ved=0ahUKEwjO7vXGjNnSAhXGf5AKHUBeBvwQ_AUIBigB&biw=1366&bih=589" 
+}
+
+});
